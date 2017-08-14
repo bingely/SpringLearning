dafcda953629703c08ee368b59b8cb534dbff18d
bingley <linmb@qq.com>
2017-08-14 16:45:46 +0800

    fix  pom

--- src/main/java/com/bingley/spring/topic/springmvc/ComandControler.java
+++ src/main/java/com/bingley/spring/topic/springmvc/ComandControler.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
  * Version:
  * Date:  2017/8/9
  */
+
+//  高版本中AbstractCommandController已经不能识别了，3.2.0还能识别
 public class ComandControler extends AbstractCommandController {
 
     protected ModelAndView handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, BindException e) throws Exception {
